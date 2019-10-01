859455341c1de9c8f5ee5117dad7ba90f8577901
shuaibixiaoyin <2275822195@qq.com>
2019-10-01 15:05:19 +0800

    commit

--- digits_sklearn.py
+++ digits_sklearn.py
@@ -1,35 +1,59 @@
 import numpy as np
 from sklearn import metrics
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans, AffinityPropagation, MeanShift, SpectralClustering, \
+    AgglomerativeClustering, DBSCAN
+from sklearn.mixture import GaussianMixture
 from sklearn.datasets import load_digits
 from sklearn.preprocessing import scale
 
+import warnings
+warnings.filterwarnings("ignore")
 
 digits = load_digits()
 data = scale(digits.data)
-
 n_samples, n_features = data.shape
 n_digits = len(np.unique(digits.target))
 labels = digits.target
 
-
 print("n_digits: %d, \t n_samples: %d, \t n_features: %d"
       % (n_digits, n_samples, n_features))
-
-print(82 * '_')
+print(50 * '_')
 print('methods\t\tNMI\t\thomo\tcompl')
 
 
-def bench_k_means(estimator, name, data):
+def evaluation(estimator, name, data):
     estimator.fit(data)
+    if name == "Gaussian mixtures":
+        labels_pred = estimator.predict(data)
+    else:
+        labels_pred = estimator.labels_
     print('%-9s\t%.3f\t%.3f\t%.3f'
-          % (name,
-             metrics.normalized_mutual_info_score(labels, estimator.labels_),
-             metrics.homogeneity_score(labels, estimator.labels_),
-             metrics.completeness_score(labels, estimator.labels_),
+          % (name.replace(' ', '\n') if ' ' in name else name,
+             metrics.normalized_mutual_info_score(labels, labels_pred),
+             metrics.homogeneity_score(labels, labels_pred),
+             metrics.completeness_score(labels, labels_pred),
              ))
 
 
-bench_k_means(KMeans(init='k-means++', n_clusters=n_digits, n_init=10),
-              name="k-means++", data=data)
+
+def main():
+    km = KMeans(init='k-means++', n_clusters=n_digits, n_init=1)
+    af = AffinityPropagation()
+    ms = MeanShift()
+    sc = SpectralClustering(affinity='nearest_neighbors', n_clusters=n_digits)
+    ward = AgglomerativeClustering(n_clusters=n_digits, linkage='ward')
+    ac = AgglomerativeClustering(linkage="average", affinity="cityblock", n_clusters=n_digits)
+    db = DBSCAN(eps=0.3)
+    gmm = GaussianMixture(n_components=n_digits, covariance_type='full')
+    method = [km, af, ms, sc, ward, ac, db, gmm]
+    name = ["k-means++", "Affinity Propagation", "Mean-Shift", "Spectral Clustering",
+            "Ward hierarchical clustering", "Agglomerative clustering", "DBSCAN", "Gaussian mixtures"]
+    for i in range(len(method)):
+        print(50 * '_')
+        evaluation(method[i], name=name[i], data=data)
+    print(50 * '_')
+
+
+if __name__ == '__main__':
+    main()
 
--- news_sklearn.py
+++ news_sklearn.py
@@ -1,11 +1,14 @@
 from sklearn.datasets import fetch_20newsgroups
 from sklearn import metrics
-from sklearn.cluster import KMeans, MiniBatchKMeans
+from sklearn.cluster import KMeans, AffinityPropagation, MeanShift, SpectralClustering, \
+    AgglomerativeClustering, DBSCAN
+from sklearn.mixture import GaussianMixture
 import numpy as np
 from sklearn.feature_extraction.text import HashingVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.pipeline import make_pipeline
-
+import warnings
+warnings.filterwarnings("ignore")
 # #############################################################################
 # Load some categories from the training set
 categories = [
@@ -15,11 +18,7 @@ categories = [
     'sci.space',
 ]
 # Uncomment the following to do the analysis on all the categories
-# categories = None
-
-print("Loading 20 newsgroups dataset for categories:")
-print(categories)
-
+#categories = None
 dataset = fetch_20newsgroups(subset='all', categories=categories,
                              shuffle=True, random_state=42)
 
@@ -30,29 +29,51 @@ print()
 labels = dataset.target
 true_k = np.unique(labels).shape[0]
 
-
 hasher = HashingVectorizer(n_features=10000,
-                                   stop_words='english', alternate_sign=False,
-                                   norm=None, binary=False)
+                           stop_words='english', alternate_sign=False,
+                           norm=None, binary=False)
 vectorizer = make_pipeline(hasher, TfidfTransformer())
 X = vectorizer.fit_transform(dataset.data)
 
 # #############################################################################
 # Do the actual clustering
-km = KMeans(n_clusters=true_k, init='k-means++', max_iter=100, n_init=1)
-print(82 * '_')
+
+print(50 * '_')
 print('methods\t\tNMI\t\thomo\tcompl')
 
-def bench_k_means(estimator, name, data):
-    estimator.fit(data)
-    print('%-9s\t%.3f\t%.3f\t%.3f'
-          % (name,
-             metrics.normalized_mutual_info_score(labels, estimator.labels_),
-             metrics.homogeneity_score(labels, estimator.labels_),
-             metrics.completeness_score(labels, estimator.labels_),
-             ))
-print()
 
-bench_k_means(km, name="k-means++", data=X)
-bench_k_means(MiniBatchKMeans(n_clusters=true_k, init='k-means++', n_init=1,
-                         init_size=1000, batch_size=1000), name="MiniBatchKMeans", data=X)
+def evaluation(estimator, name, data):
+    estimator.fit(data)
+    if name == "Gaussian mixtures":
+        labels_pred = estimator.predict(data)
+    else:
+        labels_pred = estimator.labels_
+    print('%-9s\t%.3f\t%.3f\t%.3f'
+          % (name.replace(' ', '\n') if ' ' in name else name,
+             metrics.normalized_mutual_info_score(labels, labels_pred),
+             metrics.homogeneity_score(labels, labels_pred),
+             metrics.completeness_score(labels, labels_pred),
+             ))
+
+
+
+def main():
+    km = KMeans(init='k-means++', n_clusters=true_k, n_init=1)
+    af = AffinityPropagation()
+    ms = MeanShift(bin_seeding=True)
+    sc = SpectralClustering(affinity='nearest_neighbors', n_clusters=true_k)
+    ward = AgglomerativeClustering(n_clusters=true_k, linkage='ward')
+    ac = AgglomerativeClustering(linkage="average", affinity="cityblock", n_clusters=true_k)
+    db = DBSCAN(eps=0.3)
+    gmm = GaussianMixture(n_components=true_k, covariance_type='full')
+    method = [km, af, ms, sc, ward, ac, db, gmm]
+    name = ["k-means++", "Affinity Propagation", "Mean-Shift", "Spectral Clustering",
+            "Ward hierarchical clustering", "Agglomerative clustering", "DBSCAN", "Gaussian mixtures"]
+    for i in range(len(method)):
+        print(50 * '_')
+        evaluation(method[i], name=name[i], data=X.toarray())
+    print(50 * '_')
+
+
+if __name__ == '__main__':
+    main()
